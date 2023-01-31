3d1187ca70a20c5067526f8b9009f09b02b8d587
RicCost4 <richardarcosta@gmail.com>
2023-01-31 14:02:30 -0300

    Refatorado codigo pt2

--- Inicializar Projetos Python/inicializarProjetosNextjs.py
+++ Inicializar Projetos Python/inicializarProjetosNextjs.py
@@ -1,22 +1,24 @@
 import os
 
+
+def criar_projeto(diretorio, pasta, npx):
+    print(f'criando ambiente do projeto...')
+    os.chdir(f'{str(diretorio)}\{str(pasta)}')
+    os.system(f'{str(npx)}')
+    print(f'\nambiente criando com sucesso!!')
+
+
 caminhoAtual = os.getcwd()
 npx = 'npx create-next-app'
 
 nomePasta = input("nome da pasta: ")
 if os.path.isdir(nomePasta):  # vemos de este diretorio ja existe
     print('Ja existe uma pasta com esse nome!\n')
-    print('--------------------------------\n')
-    print(f'criando ambiente do projeto...')
-    os.chdir(f'{caminhoAtual}\{nomePasta}')
-    os.system(f'{npx}')
-    print(f'\nambiente criando com sucesso!!')
+
+    criar_projeto(caminhoAtual, nomePasta, npx)
 else:  # se não, cria um
     print('criando pasta...\n')
     os.mkdir(nomePasta)
     print(f'./{nomePasta} criado com sucesso!!')
 
-    print(f'criando ambiente do projeto...')
-    os.chdir(f'{caminhoAtual}\{nomePasta}')
-    os.system(f'{npx}')
-    print(f'\nambiente criando com sucesso!!')
+    criar_projeto(caminhoAtual, nomePasta, npx)
