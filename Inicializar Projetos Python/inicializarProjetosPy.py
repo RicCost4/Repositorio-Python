f761a024540aaef0e60153b691a626a219bee3cb
Richard Costa <richardarcosta@hotmail.com>
2023-03-03 19:27:30 -0300

    Adicionado abri o vscode do novo projeto, subido varial de ativação do ambiente para a função, melhorado a barra de progresso para cada parte)

--- Inicializar Projetos Python/inicializarProjetosPy.py
+++ Inicializar Projetos Python/inicializarProjetosPy.py
@@ -7,7 +7,7 @@ def instalar_bibliotecas():  # Função desativada.
     opcao = input('Deseja instalar bibliotecas juntos?(S/N): ')
     if opcao.lower() == 's':
         try:
-            os.system(f'{activate}')
+            os.system('./venv/Scripts/Activate.bat')
             while True:
                 strPip = input('Digite o import: ')
                 # pip install pillow, pip install opencv-python, pip install pyautogui
@@ -25,7 +25,6 @@ def instalar_bibliotecas():  # Função desativada.
         except TypeError:
             print(TypeError)
 
-
 def barra_pregresso(tempoSeg: int, descricao, timeSleep: float):
     for _ in trange(tempoSeg, desc=descricao):
         time.sleep(timeSleep)
@@ -33,10 +32,10 @@ def barra_pregresso(tempoSeg: int, descricao, timeSleep: float):
 
 caminhoAtual = os.getcwd()
 venv = 'python -m venv venv'
-activate = '.\\venv\\Scripts\\Activate.bat'
-str = """
-# Abrir terminal do editor e escrever o seguinte comando:
-#    2. {activate} - ele ira ativar o ambiente virtual do seu projeto."""
+str = """# Abrir terminal do editor e escrever o seguinte comando:
+# './venv/Scripts/Activate.bat' - ele ira ativar o ambiente virtual do seu projeto.
+
+print('Hello Word!!')"""
 flags = os.O_RDWR | os.O_CREAT
 
 nomePasta = input("nome da pasta: ")
@@ -46,12 +45,12 @@ if os.path.isdir(nomePasta):  # vemos de este diretorio ja existe.
 else:  # se não, cria um.
     print('criando pasta...')
     os.mkdir(nomePasta)
-    barra_pregresso(40, 'Criando...', 0.1)
+    barra_pregresso(30, 'Criando...', 0.1)
     print(f'./{nomePasta} criado com sucesso!!\n')
 
     print(f'criando subdiretorio /{nomePasta}/src ...')
     os.mkdir(f'{nomePasta}/src')
-    barra_pregresso(40, 'Criando...', 0.1)
+    barra_pregresso(30, 'Criando...', 0.1)
     print(f'subdiretorio /{nomePasta}/src criando com sucesso!!\n')
 
     print(f'criando arquivo /{nomePasta}/main.py ...')
@@ -59,11 +58,12 @@ else:  # se não, cria um.
     os.write(criarArquivo, str.encode())
     os.lseek(criarArquivo, 0, 0)
     os.close(criarArquivo)
-    barra_pregresso(50, 'Criando...', 0.2)
+    barra_pregresso(40, 'Criando...', 0.2)
     print(f'arquivo /{nomePasta}/main.py criando com sucesso!!\n')
 
     print(f'criando ambiente virtual do projeto...')
     os.chdir(f'{caminhoAtual}\{nomePasta}')
     os.system(f'{venv}')
-    barra_pregresso(80, 'Baixando...', 0.25)
+    barra_pregresso(50, 'Baixando...', 0.25)
     print(f'\nambiente virtual do criando com sucesso!!\nProjeto criado com Sucesso')
+    os.system('code .')
